9df7cd05f6ee1125756e3ea22ac0dbf822835017
dev@steiiin.de <dev@steiiin.de>
2024-09-24 11:15:41 +0200

    minor: add connector-name to api routes

--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/barcodes', [ BarcodeController::class, 'index' ]);
+/* Lager-Barcodes-Connector -- Access */
 
 Route::get('/order-check', [OrderController::class, 'check']);
 Route::get('/order-prepare', [OrderController::class, 'prepare']);
 Route::post('/order-execute', [OrderController::class, 'execute']);
+/* Lager-Order-Connector -- Access */
