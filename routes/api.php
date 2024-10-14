38fff19bb45b4ee316846b88700f4623dcb65068
dev@steiiin.de <dev@steiiin.de>
2024-10-14 18:55:00 +0200

    patch: remove connector-names

--- routes/api.php
+++ routes/api.php
@@ -9,14 +9,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/store', [ApiStoreController::class, 'index']);
 
 Route::get('/barcodes', [ ApiBarcodeController::class, 'index' ]);
-/* Lager-Barcodes-Connector -- Access */
 
 Route::get('/order-check', [ApiOrderController::class, 'check']);
 Route::get('/order-prepare', [ApiOrderController::class, 'prepare']);
 Route::post('/order-execute', [ApiOrderController::class, 'execute']);
-/* Lager-Bestell-Konnektor -- Access */
 
 Route::get('/statistic', [ApiStatisticController::class, 'index']);
 Route::get('/logs-get', [ApiStatisticController::class, 'logs']);
 Route::post('/logs-truncate', [ApiStatisticController::class, 'truncate']);
-/* Lager-Stats-Konnektor -- Access */
