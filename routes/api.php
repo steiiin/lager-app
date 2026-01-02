d6ce4d4e6007f023b57178bb420306431ccbcfb6
dev@steiiin.de <dev@steiiin.de>
2026-01-02 13:39:34 +0100

    chore: whitespace

--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/inventory-cache', [InventoryController::class, 'cache']);
 Route::get('/inventory-jobs', [InventoryController::class, 'jobs']);
 
-Route::get('/order', [ApiOrderController::class, 'create']);
+Route::get('/order', [ ApiOrderController::class, 'create'] );
 
 Route::get('/barcodes', [ ApiBarcodeController::class, 'index' ]);
